ad20e1654c81014b2338572620f60e75e7586533
Thiag0alcantara <Thiag0alcantara@users.noreply.github.com>
2023-12-11 21:22:09 -0300

    nacionalidade

--- aula 11/ex008.js
+++ aula 11/ex008.js
@@ -1,2 +1,6 @@
 var vel = 100.5
-console.log(`a velocidade do meu carro é de ${vel}`)
+console.log(`a velocidade do seu carro é de ${vel} km/h`)
+if( vel >= 61){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`dirija sempre usando cinto de seguraça, boa viagem!`)
