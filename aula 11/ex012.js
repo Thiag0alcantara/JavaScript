841c3ff9cf87937aeeef9a539448a4d4b16df588
Thiag0alcantara <Thiag0alcantara@users.noreply.github.com>
2023-12-12 21:39:48 -0300

    votacao

---
+++ aula 11/ex012.js
@@ -0,0 +1,11 @@
+var hoje = new Date()
+var hora = hoje.getHours()
+ console.log(`agora sao exatamente ${hora} horas.`)
+ if(hora < 12){
+    console.log(`Bom Dia!`)
+}else if(hora <= 18){
+    console.log(`Boa Tarde!`)
+} else{
+    console.log(`Boa Noite!`)
+}
+    
