e5fdc1b614826699873d356d704fefc82c0abb5b
Thiag0alcantara <Thiag0alcantara@users.noreply.github.com>
2023-12-16 17:53:12 -0300

    estilo

--- aula12ex/modelo/script.js
+++ aula12ex/modelo/script.js
@@ -3,17 +3,23 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    
     msg.innerHTML = `Agora são ${hora} horas`
     
     if(hora >= 0 && hora < 12){ 
         //bom dia
         img.src = 'imagem/manha.png' 
+        document.body.style.background = '#ecec51b3'
+        
     }else if(hora >=12 && hora <= 18){
         // boa tarde
         img.src = 'imagem/tarde.png' 
+        document.body.style.background ='#ff001ea9'
+
     }else{
         //boa noite
         img.src =  'imagem/noite.png'
+        document.body.style.background ='#4a024a '
     }
         
     }
