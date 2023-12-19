f6ed092d4433fa491e9baa1484eb97296d9ecdad
Thiag0alcantara <Thiag0alcantara@users.noreply.github.com>
2023-12-19 20:30:35 -0300

    teste

---
+++ aula13/ambiente.js
@@ -0,0 +1,38 @@
+for(var c=1;c<=6;c++){
+    console.log('/+/ Vasco da Gama /+/')
+
+}
+
+
+
+
+
+/*var c = 1
+ do{
+    console.log('/+/ Vasco da Gama /+/')
+    
+    c++
+}while(c <=6)
+
+
+
+
+
+
+
+/*var c = 1
+ while(c <=6){
+    console.log('/+/ Vasco da Gama /+/')
+    
+    c++
+
+ }
+/* os quatro comandos sao iguais, vao repetir as 6 vezes, mas usando o comando while mostrado a cima fica muito menor e mais prático.
+ console.log('/+/ Vasco da Gama /+/')
+ console.log('/+/ Vasco da Gama /+/')
+ console.log('/+/ Vasco da Gama /+/')
+ console.log('/+/ Vasco da Gama /+/')
+ console.log('/+/ Vasco da Gama /+/')
+ console.log('/+/ Vasco da Gama /+/')
+ console.log('/+/ Vasco da Gama /+/')
+ */
