9c4ddf96550d8beea9a552d86dd29e879352fc4e
Thiag0alcantara <Thiag0alcantara@users.noreply.github.com>
2023-12-19 15:21:09 -0300

    .

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -9,16 +9,40 @@ function verificar(){
         var fsex = document.getElementsByTagName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
         var genero_masculino = document.getElementById('masculino')
         var genero_feminino = document.getElementById('feminino')
         if (genero_masculino.checked) {
             var genero = 'Homem'
+            if(idade >=0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'imagem/crianca-h.png')
+            }else if(idade < 25){
+                img.setAttribute('src','imagem/jovemH.jpg')
+                //jovem
+            }else if(idade < 50){
+                img.setAttribute('src','imagem/adultoH.jpg')
+                //Adulto
+
+            }else {
+                //idoso
+                img.setAttribute('src','imagem/idosoH.jpg')
+
+            }
+
+
+
         } else if (genero_feminino.checked) {
             var genero = 'Mulher'
+            
         }
+
+
         
         
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
         
