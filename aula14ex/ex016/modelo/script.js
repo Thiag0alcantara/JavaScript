19b215114f69fb2b17bab918fc9ce3b53b86fbfd
Thiag0alcantara <Thiag0alcantara@users.noreply.github.com>
2023-12-23 17:42:37 -0300

    exercicio contagem

--- aula14ex/ex016/modelo/script.js
+++ aula14ex/ex016/modelo/script.js
@@ -6,14 +6,26 @@ function contar(){
     if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ){
         window.alert('[ERRO] Faltam dados!')
     }else{
-        res.innerHTML = `contando:`
+        res.innerHTML = `contando: <br>`
         let i = Number(inicio.value) 
         let f= Number(fim.value)
         let p= Number(passo.value)
-        
-        for(let c = i; c <= f; c+= p){
-            res.innerHTML +=` ${c}  \u{1f449}`
+        if(p <= 0 ){
+            alert('passo invalido!Considerando PASSO 1')
+            p = 1
         }
+        
+        if(i < f){
+            for(let c = i; c <= f; c+= p){
+                res.innerHTML +=` ${c}  \u{1f449}`
+            }
+
+        }else{
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML +=` ${c}  \u{1f449}`
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
         
     }
         
