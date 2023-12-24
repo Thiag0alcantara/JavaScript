41489d63f34419bee059ad336eb726c9830f02cf
Thiag0alcantara <Thiag0alcantara@users.noreply.github.com>
2023-12-24 20:31:28 -0300

    .

---
+++ aula14ex/ex017/modelo/script.js
@@ -0,0 +1,9 @@
+function tabuada(){
+    let num = window.document.getElementById('txtn')
+    let tab = window.document.getElementById('seltab')
+        if(num.value.length == 0){
+            window.alert('[ERRO] Digite um número !')
+        }else{
+            let n = Number(num.value)
+        }
+}
