4ca4124dab7460e1175aac4e093ec5add81aac4f
Thiag0alcantara <Thiag0alcantara@users.noreply.github.com>
2023-12-27 17:44:36 -0300

    finalizado , projeto tabuada

--- aula14ex/ex017/modelo/script.js
+++ aula14ex/ex017/modelo/script.js
@@ -5,5 +5,13 @@ function tabuada(){
             window.alert('[ERRO] Digite um número !')
         }else{
             let n = Number(num.value)
+            let c = 1
+            tab.innerHTML = ''
+            while(c <= 10){
+                let item = document.createElement('option')
+                item.text = `${n} x ${c} = ${n*c}`
+                tab.appendChild(item)
+                c++
+            }
         }
 }
