eea736d554301a5f043981046c6e3a3bfbfa488c
Thiag0alcantara <Thiag0alcantara@users.noreply.github.com>
2023-12-28 12:57:34 -0300

    .

--- aula14ex/ex017/modelo/script.js
+++ aula14ex/ex017/modelo/script.js
@@ -1,6 +1,7 @@
 function tabuada(){
     let num = window.document.getElementById('txtn')
     let tab = window.document.getElementById('seltab')
+    
         if(num.value.length == 0){
             window.alert('[ERRO] Digite um número !')
         }else{
