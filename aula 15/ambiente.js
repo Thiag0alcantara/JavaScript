aeffb0811fad36a36f8a810e91da22efbec6f661
Thiag0alcantara <Thiag0alcantara@users.noreply.github.com>
2023-12-28 10:00:07 -0300

    array, variaveis compostas

---
+++ aula 15/ambiente.js
@@ -0,0 +1,7 @@
+let num =[5,8,2,9,3]
+    num.push(4)//push adiciona um vetor 
+    num.sort() //sort deixa os vetores em ordem
+    console.log(`Nosso vetor é o ${num}`)
+    console.log(num[3])
+    console.log(`o primeiro vetor é o ${num[0]}`)
+    
